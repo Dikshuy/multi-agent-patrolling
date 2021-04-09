9609f153b78b5342a7d3586ef9a63988ff099695
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-04-09 16:37:10 +0530

    without_intent_cr remaining maps ready to be trained

--- scripts/config_simulate.py
+++ scripts/config_simulate.py
@@ -6,7 +6,7 @@ import rospkg
 import glob
 
 dir_name = rospkg.RosPack().get_path('mrpp_sumo')
-config_files = glob.glob(dir_name + '/config/with_intenwithout_intent_cr/without_intent_cr*.yaml')
+config_files = glob.glob(dir_name + '/config/without_intent_cr/without_intent_cr*.yaml')
 count = 0
 for conf in config_files:
     os.system('xterm -e "{}/tpbp.sh" {}'.format(dir_name, conf))
