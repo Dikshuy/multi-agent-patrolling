9d9810e8d4cb480622c2e33cf007497b9168bed7
vishwajeetiitb <vishwajeet724728@gmail.com>
2021-04-09 12:14:59 +0530

    remaining maps for with intent

--- tpbp.sh
+++ tpbp.sh
@@ -4,7 +4,7 @@ xterm -e "rosparam load $1" &
 sleep 2
 xterm -e "rosrun mrpp_sumo sumo_wrapper.py" &
 sleep 3
-xterm -e "rosrun mrpp_sumo without_intent_reactive_flag.py" &
+xterm -e "rosrun mrpp_sumo with_intent.py" &
 sleep 2
 xterm -e "rosrun mrpp_sumo command_center.py" 
 sleep 10
